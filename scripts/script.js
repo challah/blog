64a02157aa78c50461fa2c225cc5b0ec8da30ea6
challah <yang.bread@gmail.com>
2021-10-12 23:31:31 -0400

    update with taskbar

--- scripts/script.js
+++ scripts/script.js
@@ -41,3 +41,23 @@ function changeLabel() {
     switchLabel.innerHTML = "Turn off the lights";
   }
 }
+
+
+// CLOCK -------------------------
+function startTime() {
+  const today = new Date();
+  let h = today.getHours();
+  let m = today.getMinutes();
+  m = checkTime(m);
+  document.querySelector(".time").innerHTML = h + ":" + m;
+  setTimeout(startTime, 1000);
+}
+
+function checkTime(i) {
+  if (i < 10) {
+    i = "0" + i;
+  }
+  return i;
+}
+
+document.body.onload = startTime();
